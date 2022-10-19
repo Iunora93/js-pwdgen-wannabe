c1ab10ca2d84c16fdc143e59c1da880e3904da75
Iunora93 <eleonoraruggieri22@gmail.com>
2022-10-19 15:39:44 +0200

    Prompt firstname, lastname, color

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+/* 
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito76 
+*/
+
+/* Chiedi all'utente il suo nome */
+const firstName = prompt("Ciao inserisci il tuo nome");
+console.log(firstName);
+
+/* Chiedi all'utente il suo cognome */
+const lastName = prompt("Inserisci il tuo cognome");
+console.log(lastName);
+
+/* Chiedi all'utente il suo colore preferito */
+const favoriteColor = prompt("Inserisci il tuo colore preferito");
+console.log(favoriteColor);
+
+/* Scrivi sulla pagina nomecognomecolorepreferito76 */
+const totalInformation = ``;
+console.log(totalInformation);
+
